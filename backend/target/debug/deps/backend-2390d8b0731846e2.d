81fce3dddf45cca0bb4d9f88e9bf12630379ea99
JU-NINE NGU CHO <chojuninengu@gmail.com>
2025-04-02 10:26:39 +0100

    Delete backend/target directory

--- backend/target/debug/deps/backend-2390d8b0731846e2.d
+++
@@ -1,5 +0,0 @@
-/home/ju-nine/projects/personal_projects/motivation-app/backend/target/debug/deps/backend-2390d8b0731846e2: src/main.rs
-
-/home/ju-nine/projects/personal_projects/motivation-app/backend/target/debug/deps/backend-2390d8b0731846e2.d: src/main.rs
-
-src/main.rs:
